3cf26405867b38523c9f1d910f7712a98ed386d0
Flavio-Ore <pseudominio.go@gmail.com>
2023-11-10 11:40:07 -0500

    Initial commit: Project Setup

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+async function bootstrap () {
+  const app = await NestFactory.create(AppModule)
+
+  // Do validations
+
+  app.setGlobalPrefix('api/v1')
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //if the request has a property that is not in the DTO, it will be ignored
+      forbidNonWhitelisted: true, //if the request has a property that is not in the DTO, it will throw an error
+      transform: true, //transform the request body to the DTO type
+    }),
+  )
+
+  await app.listen(3000)
+}
+bootstrap()
